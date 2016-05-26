4bf7e2cc1515efa81a5badba7e6a0090590c39e7
Marcos Ramos <mrsilvatads@gmail.com>
2016-05-26 13:04:26 -0300

    Crie a Classe CategoriaModel

--- BeautySystem/Model/DataModel/CategoriaModel.cs
+++ BeautySystem/Model/DataModel/CategoriaModel.cs
@@ -9,5 +9,31 @@ namespace BeautySystem.Model.DataModel
     public class CategoriaModel
     {
 
+        public CategoriaModel()
+        {
+            CodCat = 0;
+            NomeCat = "";
+        }
+
+        public CategoriaModel(int codcat, String nomecat)
+        {
+            CodCat = codcat;
+            NomeCat = nomecat;
+        }
+
+        private int cod_cat;
+        public int CodCat
+        {
+            get{return cod_cat;}
+            set{cod_cat = value;}
+        }
+
+        public String nome_cat;
+        public String NomeCat
+        {
+            get { return nome_cat; }
+            set { nome_cat = value; }
+        }
     }
-}
+    }
+
