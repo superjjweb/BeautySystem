760997f231d49dfb29d63037c40495641013e097
Jose Joaquim <superjjweb09@gmail.com>
2016-05-26 02:37:50 -0300

    Configuração das instruções SQL para a tabela Categoria

--- BeautySystem/Model/Repository/CategoriaRepository.cs
+++ BeautySystem/Model/Repository/CategoriaRepository.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+
 
 namespace BeautySystem.Model.Repository
 {
-    class CategoriaRepository
+    public class CategoriaRepository
     {
+        public string strConexao = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        //Variaveis Constantes para instrução SQL
+
+        public const string strDelete = "DELETE from categoria WHERE cod_cat = @cod_cat";
+        public const string strInsert = "INSERT INTO categoria VALUES (@nome_cat)";
+        public const string strSelect = "SELECT * FROM categoria";
+        public const string strUpdate = "UPDATE categoria SET nome_cat = @nome_cat";
     }
+
 }
+
