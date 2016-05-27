199bb54c8bb102fa373d876560a5e4fc3cd8833b
Marcos Ramos <mrsilvatads@gmail.com>
2016-05-27 16:44:58 -0300

    Alteração da Tela de categoria

--- BeautySystem/View/frmCadastroCategoria.Designer.cs
+++ BeautySystem/View/frmCadastroCategoria.Designer.cs
@@ -28,62 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtNomeCat = new System.Windows.Forms.TextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabConsulta = new System.Windows.Forms.TabPage();
+            this.tabCadastro = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNomeCat = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabCadastro.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 44);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Nome da Categoria";
-            // 
-            // txtNomeCat
-            // 
-            this.txtNomeCat.Location = new System.Drawing.Point(12, 64);
-            this.txtNomeCat.Name = "txtNomeCat";
-            this.txtNomeCat.Size = new System.Drawing.Size(302, 20);
-            this.txtNomeCat.TabIndex = 1;
             // 
             // btnSalvar
             // 
             this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalvar.Image = global::BeautySystem.Properties.Resources.laptop_accept;
             this.btnSalvar.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnSalvar.Location = new System.Drawing.Point(12, 141);
+            this.btnSalvar.Location = new System.Drawing.Point(6, 17);
             this.btnSalvar.Name = "btnSalvar";
-            this.btnSalvar.Size = new System.Drawing.Size(83, 80);
+            this.btnSalvar.Size = new System.Drawing.Size(83, 69);
             this.btnSalvar.TabIndex = 2;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(396, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Nova Categoria";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(250, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Procurar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Pesquisar Categoria";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(5, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabConsulta);
+            this.tabControl1.Controls.Add(this.tabCadastro);
+            this.tabControl1.Location = new System.Drawing.Point(2, 59);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(519, 268);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabConsulta
+            // 
+            this.tabConsulta.Location = new System.Drawing.Point(4, 22);
+            this.tabConsulta.Name = "tabConsulta";
+            this.tabConsulta.Padding = new System.Windows.Forms.Padding(3);
+            this.tabConsulta.Size = new System.Drawing.Size(511, 242);
+            this.tabConsulta.TabIndex = 0;
+            this.tabConsulta.Text = "Consulta Categoria";
+            this.tabConsulta.UseVisualStyleBackColor = true;
+            // 
+            // tabCadastro
+            // 
+            this.tabCadastro.Controls.Add(this.panel1);
+            this.tabCadastro.Controls.Add(this.label3);
+            this.tabCadastro.Controls.Add(this.txtNomeCat);
+            this.tabCadastro.Location = new System.Drawing.Point(4, 22);
+            this.tabCadastro.Name = "tabCadastro";
+            this.tabCadastro.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCadastro.Size = new System.Drawing.Size(511, 242);
+            this.tabCadastro.TabIndex = 1;
+            this.tabCadastro.Text = "Cadastro";
+            this.tabCadastro.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.btnSalvar);
+            this.panel1.Location = new System.Drawing.Point(4, 150);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 89);
+            this.panel1.TabIndex = 2;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(296, 22);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Fechar";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(215, 22);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Excluir";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(134, 22);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Editar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Nome da Categoria";
+            // 
+            // txtNomeCat
+            // 
+            this.txtNomeCat.Location = new System.Drawing.Point(6, 23);
+            this.txtNomeCat.Name = "txtNomeCat";
+            this.txtNomeCat.Size = new System.Drawing.Size(302, 20);
+            this.txtNomeCat.TabIndex = 1;
+            // 
             // frmCadastroCategoria
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 335);
-            this.Controls.Add(this.btnSalvar);
-            this.Controls.Add(this.txtNomeCat);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(803, 482);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tabControl1);
             this.Name = "frmCadastroCategoria";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de Categoria";
+            this.tabControl1.ResumeLayout(false);
+            this.tabCadastro.ResumeLayout(false);
+            this.tabCadastro.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtNomeCat;
         private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabConsulta;
+        private System.Windows.Forms.TabPage tabCadastro;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNomeCat;
     }
 }
